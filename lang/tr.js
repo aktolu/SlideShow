d8ff992a71f07cf42b7c10c276d039bdb0bf98ff
Muhammed Aktolu <aktolu@gmail.com>
2020-07-03 11:50:16 +0300

    Add tr.js

---
+++ lang/tr.js
@@ -0,0 +1,41 @@
+﻿/*
+Copyright (c) 2003-2013, Cricri042. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+Targeted for "ad-gallery" JavaScript : http://adgallery.codeplex.com/
+And "Fancybox" : http://fancyapps.com/fancybox/
+*/
+CKEDITOR.plugins.setLang( 'slideshow', 'tr', {
+	toolbar	: 'SlideShow',
+	dialogTitle : 'SlideShow properties',
+	fakeObject : 'SlideShow',
+	imgList : 'Resim',
+	imgAdd : 'Bir Resim Ekle',
+	imgTitle : 'Başlık',
+	imgDelete : 'Sil',
+	imgEdit : 'Düzenle',
+    	validModif : 'Değişiklikleri Kaydet',
+	previewMode : 'Önizleme',
+	imgDesc : 'Kısa Açıklama',
+	editSlideShow : 'SlideShow\'u Düzenle',
+	picturesList : "Resim Listesi :",
+	insertSlideShow : 'SlideShow Ekle',
+	showThumbs : 'Küçük resimleri göster',
+	showTitle : 'Başlığı Göster',
+	showControls : "Başlat / Durdur butonu göster",
+	labelStart : "Başlat",
+	labelStop : "Durdur",
+	arrowUp : "\u2191",
+	arrowDown : "\u2193",
+	displayTime : 'Gör. Sür. (San.)',
+	transitionTime : 'Geç. Sür. (mSan.)',
+	autoStart : 'OtoBaşlat',
+	pictHeight : 'Yükseklik (px)',
+	pictWidth : 'Genişlik (px)',
+	openOnClick : 'Tıklandığında Aç',
+	transition : 'Geçiş Tipi',
+	tr1 : 'Yok',
+	tr2 : 'Boyutlandır',
+	tr3 : 'Dikey Geçiş',
+	tr4 : 'Yatay Geçiş',
+	tr5 : 'Karart',
+});
